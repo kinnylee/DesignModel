99027197044f98be9aab2566de128a9d1aa14bf9
lijl-c <lijl-c@glodon.com>
2016-08-13 16:56:31 +0800

    singleton model

---
+++ src/com/kinnylee/designmodel/dm/LazySingleton.java
@@ -0,0 +1,16 @@
+package com.kinnylee.designmodel.dm;
+
+/**
+ * Created by lijl-c on 2016/8/13.
+ */
+public class LazySingleton {
+
+    private static LazySingleton m_instance = null;
+
+    public static LazySingleton  getInstance(){
+        if(null == m_instance){
+            m_instance = new LazySingleton();
+        }
+        return  m_instance;
+    }
+}
